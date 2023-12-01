fc8d5712bb53d33deedb1fbecb2ec9d602a62d07
tktyo <zhaohaichao24@outlook.com>
2023-12-01 16:00:48 +0900

    modify lc01 solution

--- HashTable/Lc1.java
+++ HashTable/Lc1.java
@@ -1,5 +1,21 @@
 package com.lc.HashTable;
-
+import java.util.HashMap;
+//two sum
 public class Lc1 {
-    
+    public int[] twoSum(int[] nums,int target){
+        int[] result=new int[2];
+        HashMap<Integer, Integer> map= new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int j=target-nums[i];
+            if(map.containsKey(j)){
+                result[0]=i;
+                result[1]=map.get(j);
+                break;
+            }
+            else{
+                map.put(nums[i],i);
+            }
+        }
+        return result;
+    }
 }
