9bb2ce41256260455c20379fc2bd68963f496990
tktyo <zhaohaichao24@outlook.com>
2024-01-22 13:58:47 +0900

    add Lc102 solution

---
+++ BinaryTree/Lc102.java
@@ -0,0 +1,36 @@
+package com.lc.BinaryTree;
+
+import com.sun.source.tree.Tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+//level order traversal
+public class Lc102 {
+    List<List<Integer>> result= new ArrayList<>();
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        levelOrderTraversal(root);
+        return result;
+    }
+    public void levelOrderTraversal(TreeNode root){
+        if(root==null)
+            return;
+        Queue<TreeNode> queue=new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            List<Integer> list=new ArrayList<>();
+            int size=queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node=queue.poll();
+                list.add(node.val);
+                if(node.left!=null)
+                    queue.add(node.left);
+                if(node.right!=null)
+                    queue.add(node.right);
+            }
+            result.add(list);
+        }
+    }
+}
