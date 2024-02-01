713f2f0dd27cf7af9f2a8eaaaf4c4b38801ddadc
tktyo <zhaohaichao24@outlook.com>
2024-02-01 14:52:35 +0900

    add Lc104 solution

---
+++ BinaryTree/Lc104.java
@@ -0,0 +1,32 @@
+package com.lc.BinaryTree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Lc104 {
+    public int maxDepth(TreeNode root) {
+        return traversal(root);
+    }
+    public int traversal(TreeNode root){
+        int depth=0;
+        Queue<TreeNode> queue=new LinkedList<>();
+        if(root==null)
+            return 0;
+        queue.add(root);
+        while(!queue.isEmpty()){
+            int size=queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node=queue.poll();
+                if(node.left!=null){
+                    queue.add(node.left);
+                }
+                if(node.right!=null){
+                    queue.add(node.right);
+                }
+            }
+            depth++;
+        }
+        return depth;
+
+    }
+}
